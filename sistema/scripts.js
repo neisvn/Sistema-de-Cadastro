1db9c2178ddf774d9e005b29690cc5f14b066de0
Nathan <nathansilvamachado@gmail.com>
2023-12-19 11:11:06 -0300

    Implementada lógica inicial do Login

--- sistema/scripts.js
+++ sistema/scripts.js
@@ -1,29 +1,17 @@
-function logar() {
-  var email = document.getElementById("email").value;
-  var senha = document.getElementById("senha").value;
-  if (email == "nathansilvamachado@gmail.com" && senha == "1234") {
-    location.href = "home.html";
-  } else {
-    swal(
-      "E-mail ou senha incorretos!",
-      "Caso tenha esquecido a sua senha clique em Recuperar Senha",
-      "error"
-    );
-  }
-}
-
 function openCadastroModal() {
   const modal = document.querySelector("dialog");
   modal.showModal();
 }
+
 function fechaModal() {
   const modal = document.querySelector("dialog");
   modal.close();
 }
+
 const botaoCancelar = document.getElementById("cancelar");
 const buttonCad = document.getElementById("buttonCad");
 buttonCad.addEventListener("click", openCadastroModal);
-botaoCancelar.addEventListener("click", fechaModal());
+botaoCancelar.addEventListener("click", fechaModal);
 
 function validaCampo() {
   const emailValido = isEmailvalid();
@@ -31,6 +19,7 @@ function validaCampo() {
   const senhaValida = isPasswordValid();
   document.getElementById("btn-logar").disabled = !emailValido || !senhaValida;
 }
+
 function isEmailvalid() {
   const email = document.getElementById("email").value;
   if (!email) {
@@ -38,16 +27,16 @@ function isEmailvalid() {
   }
   return validaEmail(email);
 }
+
 function isPasswordValid() {
-  const password = document.getElementById("senha").value;
-  if (!password) {
-    return false;
-  }
-  return true;
+  const senha = document.getElementById("senha").value;
+  return senha.length > 0;
 }
+
 function validaEmail(email) {
   return /\S+@\S+/.test(email);
 }
+
 function recuperarSenha() {
   const email = document.getElementById("email").value;
   swal({
@@ -59,4 +48,23 @@ function checkEnter(event) {
   if (event.key === "Enter") {
     return logar();
   }
+}
+
+function logar() {
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, senha)
+    .then((response) => {
+      location.href = "home.html";
+    })
+    .catch((error) => {
+      console.log("erro: ", error);
+      swal(
+        "E-mail ou senha incorretos!",
+        "Caso tenha esquecido a sua senha clique em Recuperar Senha",
+        "error"
+      );
+    });
 }
